3e371bde1d6ed45c191891f9b90eeb41f52dfe6c
kdichen <kdichen@163.com>
2018-05-25 16:59:29 +0800

    用拦截器方式,重置session时间(只要是.do结尾的都会进入拦截器)

---
+++ src/main/java/com/chenqian/controller/common/SessionExpireFilter.java
@@ -0,0 +1,48 @@
+package com.chenqian.controller.common;
+
+import com.chenqian.common.Const;
+import com.chenqian.pojo.User;
+import com.chenqian.util.CookieUtil;
+import com.chenqian.util.JsonUtil;
+import com.chenqian.util.RedisPoolUtil;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author: 陈谦
+ * @Date: 2018/5/25 16:45
+ */
+
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            // 如果loginToken不为空或空字符串, 则从redis中获取信息
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr, User.class);
+            if (user != null) {
+                //如果user不为空，则重置session的时间，即调用expire命令
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExTime.REDIS_SESSION_EX_TIME);
+
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
