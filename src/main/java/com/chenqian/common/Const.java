56f302a1851fa8d7ebdba3f91171de5219784bd0
kdichen <kdichen@163.com>
2018-05-27 19:20:44 +0800

    Redis分布式锁, 双重防止死锁

--- src/main/java/com/chenqian/common/Const.java
+++ src/main/java/com/chenqian/common/Const.java
@@ -168,4 +168,9 @@ public class Const {
         }
 
     }
+
+    public interface  REDIS_LOCK{
+        // 关闭订单的分布式锁
+        String CLOSE_ORDER_TASK_LOCK = "CLOSE_ORDER_TASK_LOCK";
+    }
 }
--- src/main/java/com/chenqian/task/CloseOrderTask.java
+++ src/main/java/com/chenqian/task/CloseOrderTask.java
@@ -1,9 +1,13 @@
 package com.chenqian.task;
 
+import com.chenqian.common.Const;
 import com.chenqian.service.IOrderService;
 import com.chenqian.util.PropertiesUtil;
+import com.chenqian.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,15 +23,53 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
-    /**
-     * 每一分钟执行一次, 关闭从当前时间开始之前两个小时下单但并为付款的订单
-     */
-    //    @Scheduled(cron="0 */1 * * * ?")//每1分钟(每个1分钟的整数倍)
-    public void closeOrderTaskV1(){
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void closeOrderTaskV3() {
         log.info("关闭订单定时任务启动");
-        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
-        //iOrderService.closeOrder(hour);
+        // 设置一个分布式锁, 锁的时间为5秒. 锁用当前时间+lockTimeout(5秒)
+        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "5000"));
+        Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+        if (setnxResult != null && setnxResult.intValue() == 1) {
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        } else {
+            // 未获取到锁，继续判断，判断时间戳，看是否可以重置并获取到锁
+            String lockValueStr = RedisShardedPoolUtil.get(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            if (lockValueStr != null && System.currentTimeMillis() > Long.parseLong(lockValueStr)) {
+                // 走到这里, 代表可以重新获取锁. 重新设置所. getset,会返回一个旧值, 但是在这里要返回最新的旧值
+                String getSetResult = RedisShardedPoolUtil.getSet(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+                //再次用当前时间戳getset。
+                //返回给定的key的旧值，->旧值判断，是否可以获取锁
+                //当key没有旧值时，即key不存在时，返回nil ->获取锁
+                //这里我们set了一个新的value值，获取旧的值。
+                if (getSetResult == null || (getSetResult != null && StringUtils.equals(lockValueStr, getSetResult))) {
+                    //真正获取到锁
+                    closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                } else {
+                    log.info("没有获取到分布式锁:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                }
+            } else {
+                log.info("没有获取到分布式锁:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            }
+        }
         log.info("关闭订单定时任务结束");
+    }
+
+
+    /**
+     * 设置锁的有效期
+     *
+     * @param lockName 锁的名字
+     */
+    private void closeOrder(String lockName) {
+        //有效期50秒，防止死锁
+        RedisShardedPoolUtil.expire(lockName, 5);
+        log.info("获取{},当前线程的名字:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour", "2"));
+        iOrderService.closeOrder(hour);
+        // 删除锁
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        log.info("释放{},当前线程的名字:{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, Thread.currentThread().getName());
+        log.info("===============================");
     }
 
 
